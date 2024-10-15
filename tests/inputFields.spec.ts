36360a09ff5778b0c29408189640339e01f9c900
jradivojevic <radivojevicjelena@outlook.com>
2024-10-14 20:45:01 -0400

    Imput Fileds homeworks improved

--- tests/inputFields.spec.ts
+++ tests/inputFields.spec.ts
@@ -1,58 +1,54 @@
 import { test, expect } from '@playwright/test';
 
 
-test.describe('Input fields', () => {  
-test.beforeEach( async({page}) => {
-  await page.goto('/')
-  await page.getByRole('link', { name: 'Pet Types' }).click();
-  await page.getByRole('heading', { name: 'Pet Types' }).click()
-})
+test.describe('Input fields', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('/')
+    await page.getByRole('link', { name: 'Pet Types' }).click();
+    await expect(page.getByRole('heading', { name: 'Pet Types' })).toHaveText('Pet Types');
+  })
 
-test('Update pet type', async ({page}) => {
-const tableRow = page.locator('tbody tr')
-await tableRow.getByRole('button', {name: 'Edit'}).first().click()
-await expect(page.getByRole('heading')).toContainText('Edit Pet Type');
-const nameInputField = page.locator('#name')
-await nameInputField.click();
-await nameInputField.clear()
-await nameInputField.fill('rabbit')
-const updateButton = page.getByRole('button', {name: 'Update'})
-await updateButton.click()
-await expect(tableRow.locator('td input[id="0"]')).toHaveValue('rabbit')
-await tableRow.getByRole('button', {name: 'Edit'}).first().click()
-await nameInputField.click();
-await nameInputField.clear()
-await nameInputField.fill('cat')
-await updateButton.click()
-await expect(tableRow.locator('td input[id="0"]')).toHaveValue('cat')
+  test('Update pet type', async ({ page }) => {
+    const firstTableRow = page.locator('tbody tr').first()
+    await firstTableRow.getByRole('button', { name: 'Edit' }).click()
+    await expect(page.getByRole('heading')).toHaveText('Edit Pet Type');
+    const nameInputField = page.locator('#name')
+    await nameInputField.click();
+    await nameInputField.clear()
+    await nameInputField.fill('rabbit')
+    const updateButton = page.getByRole('button', { name: 'Update' })
+    await updateButton.click()
+    await expect(firstTableRow.locator('[id="0"]')).toHaveValue('rabbit')
+    await firstTableRow.getByRole('button', { name: 'Edit' }).first().click()
+    await nameInputField.click();
+    await nameInputField.clear()
+    await nameInputField.fill('cat')
+    await updateButton.click()
+    await expect(firstTableRow.locator('[id="0"]')).toHaveValue('cat')
 
-});
+  });
 
-test('Cancel pet type update', async ({ page }) => {
-  const editDogRow = page.getByRole('button', { name: 'Edit'}).nth(1);
-  await editDogRow.click()
-  const inputFieldDog = page.locator('#name')
-  await inputFieldDog.click()
-  await inputFieldDog.clear()
-  await inputFieldDog.fill('moose')
-  await expect(inputFieldDog).toHaveValue('moose')
-  const cancelButton = page.getByRole('button', {name: 'Cancel'})
-  await cancelButton.click()
-  const tableRow = page.locator('tbody tr')
-  await expect(tableRow.locator('td input[id="1"]')).toHaveValue('dog')
-  
-});
+  test('Cancel pet type update', async ({ page }) => {
+    await page.getByRole('button', { name: 'Edit' }).nth(1).click()
+    const nameInputField = page.locator('#name')
+    await nameInputField.click()
+    await nameInputField.clear()
+    await nameInputField.fill('moose')
+    await expect(nameInputField).toHaveValue('moose')
+    await page.getByRole('button', { name: 'Cancel' }).click()
+    const tableRow = page.locator('tbody tr')
+    await expect(page.locator('[id="1"]')).toHaveValue('dog')
 
-test('Pet type name is required validation', async ({ page }) => {
-  const editLizardRow = page.getByRole('button', { name: 'Edit'}).nth(2);
-  await editLizardRow.click()
-  const inputLizardRow = page.locator('#name')
-  await inputLizardRow.click()
-  await inputLizardRow.clear()
-  const ErrorMessage = page.locator('span.help-block')
-  await expect(ErrorMessage).toHaveText('Name is required');
-  
-});
+  });
+
+  test('Pet type name is required validation', async ({ page }) => {
+    await page.getByRole('button', { name: 'Edit' }).nth(2).click()
+    const nameInputField = page.locator('#name')
+    await nameInputField.click()
+    await nameInputField.clear()
+    await expect(page.locator('.help-block')).toHaveText('Name is required');
+
+  });
 
 
 })
