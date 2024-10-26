03e6c14a86092beba391fbe3d3f272f834c89d19
jradivojevic <radivojevicjelena@outlook.com>
2024-10-25 22:26:28 -0400

    Lists and Drop Downs: improved homework assigment

--- tests/listsAndDropDowns.spec.ts
+++ tests/listsAndDropDowns.spec.ts
@@ -4,15 +4,18 @@ test.describe('Lists and Drop Downs', () => {
     //declare variables
     let petTypeInputField
     let petNameInputField
+    let petTypeField
 
     test.beforeEach(async ({ page }) => {
         await page.goto('/')
         await page.getByRole('button', { name: 'Owners' }).click()
         await page.getByRole('link', { name: 'Search' }).click()
-        const ownersButton = page.locator('.dropdown', {hasText: "Search"})
-        await expect(ownersButton).toContainText('Owners')
+        //const ownersButton = page.locator('.dropdown', {hasText: "Search"})
+        //await expect(ownersButton).toContainText('Owners')
+        await expect(page.getByRole('heading', { name: 'Owners' })).toHaveText('Owners');
         petTypeInputField = page.locator('#type')
         petNameInputField = page.locator('#name')
+        petTypeField = page.locator('#type1')
     })
 
     test('Test Case 1: Validate selected pet types from the list', async ({ page }) => {
@@ -21,13 +24,13 @@ test.describe('Lists and Drop Downs', () => {
         await page.getByRole('button', { name: 'Edit Pet' }).click()
         await expect(petNameInputField).toHaveValue('Leo')
         await expect(page.locator('#owner_name')).toHaveValue('George Franklin')
-        await expect(petTypeInputField).toHaveValue('cat')
+        await expect(petTypeField).toHaveValue('cat')
         await petTypeInputField.click()
         const typesOfPets = ['cat', 'dog', 'lizard', 'snake', 'bird', 'hamster']
 
         for (const pet of typesOfPets) {
             await petTypeInputField.selectOption(pet)
-            await expect(petTypeInputField).toHaveValue(pet)
+            await expect(petTypeField).toHaveValue(pet)
             await petTypeInputField.click()
         }
     })
@@ -37,20 +40,21 @@ test.describe('Lists and Drop Downs', () => {
         const editButton = page.locator('.dl-horizontal', { hasText: "Rosy" }).getByRole('button', { name: 'Edit Pet' })
         await editButton.click()
         await expect(petNameInputField).toHaveValue('Rosy')
-        await expect(petTypeInputField).toHaveValue('dog')
+        await expect(petTypeField).toHaveValue('dog')
         await petTypeInputField.click()
         await petTypeInputField.selectOption('bird')
+        await expect(petTypeField).toHaveValue('bird')
         await expect(petTypeInputField).toHaveValue('bird')
         const updateButton = page.getByRole('button', { name: 'Update Pet' })
         await updateButton.click()
-        await expect(petNameInputField).toHaveValue('Rosy')
-        await expect(petTypeInputField).toHaveValue('bird')
+        await expect(page.locator('.dl-horizontal', { hasText: "Rosy" })).toContainText('bird')
         await editButton.click()
         await petTypeInputField.selectOption('dog')
         await expect(petTypeInputField).toHaveValue('dog');
+        await expect(petTypeField).toHaveValue('dog');
         await updateButton.click()
-        await expect(petNameInputField).toHaveValue('Rosy')
-        await expect(petTypeInputField).toHaveValue('dog')
+        await expect(page.locator('.dl-horizontal', { hasText: "Rosy" })).toContainText('dog')
+      
 
     })
 
