a99322f1d912f58ae1d1d103383c3bc82869a74a
jradivojevic <radivojevicjelena@outlook.com>
2024-10-26 21:40:27 -0400

    Dialog boxes - Homework Assignment completed

---
+++ tests/dialogBoxes.spec.ts
@@ -0,0 +1,25 @@
+import test, { expect } from "@playwright/test"
+
+
+test.describe('Dialog boxes', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto("/")
+    })
+
+    test('Test Case: Add and delete pet type', async ({ page }) => {
+        await page.getByRole('link', { name: 'Pet Types' }).click()
+        await expect(page.getByRole('heading')).toHaveText('Pet Types')
+        await page.getByRole('button', { name: "Add" }).click()
+        await expect(page.locator('#name')).toBeVisible()
+        await expect(page.locator('label')).toBeVisible()
+        await page.locator('#name').fill('pig')
+        await page.getByRole('button', { name: 'Save' }).click()
+        await expect(page.locator('table input').last()).toHaveValue('pig')
+        page.on('dialog', dialog => {
+            expect(dialog.message()).toEqual('Delete the pet type?')
+            dialog.accept()
+        })
+        await page.getByRole('button', { name: 'Delete' }).last().click()
+        await expect(page.getByRole('textbox').last()).not.toHaveValue('pig')
+    })
+})
